738bd0266d3b9e01bbbe70997bd2d1e832a492b7
Dan Chen <dchen@thoughtworks.com>
2016-07-11 15:06:02 +0800

    first week first page

--- gulpfile.js
+++ gulpfile.js
@@ -19,16 +19,21 @@ gulp.task('sass', function () {
       stream: true
     }));
 });
+
+gulp.task('images', function () {
+  return gulp.src('./src/images/**')
+    .pipe(gulp.dest('./dist/images'));
+});
  
 
-gulp.task('serve', ['browserSync','sass'], function() {
+gulp.task('serve', ['browserSync','sass','images'], function() {
   //2. serve at custom port
   var server = gls.static('./', 3030);
   // var server = gls('./', true, 3030);
   server.start();
 
   //use gulp.watch to trigger server actions(notify, start or stop)
-  gulp.watch(['./src/*.scss', './index.html', './src/images/***'], ['sass'], function (file) {
+  gulp.watch(['./src/*.scss', './index.html', './src/images/***'], ['sass', 'images'], function (file) {
     server.notify.apply(server, [file]);
   });
 });
